e0b987f17830a40b25d7ac41905a6a502466615b
root <root@vultr.guest>
2019-09-26 17:36:15 +0800

    aa

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker run --name v2ray \
--v $PWD/Caddyfile:/etc/Caddyfile \
+docker run --name v2ray -d \
+-p 443:443 \
+-p 80:80 \
+-v $PWD/Caddyfile:/srv/Caddyfile \
 -v $PWD/v2ray.json:/etc/v2ray/config.json \
-v2ray
+-v $PWD/caddy.sh:/caddy.sh \
+pengchujin/v2ray_ws:0.08
